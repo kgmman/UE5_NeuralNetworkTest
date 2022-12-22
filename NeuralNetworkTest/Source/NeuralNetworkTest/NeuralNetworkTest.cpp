861c15f06b450549100d16f956ca01b50db518ac
Kim Bong Soo <kgmman@evrstudio.com>
2022-12-22 16:27:20 +0900

    create project

---
+++ NeuralNetworkTest/Source/NeuralNetworkTest/NeuralNetworkTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NeuralNetworkTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NeuralNetworkTest, "NeuralNetworkTest" );
---
+++ NeuralNetworkTest/Source/NeuralNetworkTest/NeuralNetworkTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NeuralNetworkTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEURALNETWORKTEST_API ANeuralNetworkTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
